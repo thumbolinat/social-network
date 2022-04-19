dffd1be14a7c69957c1bf09de26dc4812a552c5b
thumbolinat <93739066+thumbolinat@users.noreply.github.com>
2022-04-19 03:17:42 -0400

    updated controllers

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
